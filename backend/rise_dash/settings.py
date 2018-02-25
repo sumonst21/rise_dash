a207095402b0cfb16d25b4f165245617fd3b8537
joel saunders <joelsaunders@ueni.com>
2018-02-25 19:09:52 +0000

    remove session auth

--- backend/rise_dash/settings.py
+++ backend/rise_dash/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
