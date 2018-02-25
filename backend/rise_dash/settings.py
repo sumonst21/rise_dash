9d996892c5ee0b19695a3fbae72bf6117f242c45
joel saunders <joelsaunders@ueni.com>
2018-02-25 15:50:27 +0000

    add password to db

--- backend/rise_dash/settings.py
+++ backend/rise_dash/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': os.getenv('DB_PORT', 3306)
     }
